6021beaaa9ccfff8bc00dc1bca47277bf5e349a2
Rohan Singh <rohansingh@Rohans-MacBook-Pro.local>
2022-10-13 23:23:40 -0400

    Added on to the first script, and just added some new functions to show output

--- Intermediate JS/MyScript_1.js
+++ Intermediate JS/MyScript_1.js
@@ -1,3 +1,24 @@
 function clicked(){
     document.getElementById("demo").innerHTML = "Psych!";
 }
+
+function piracyMessage(){
+    //Note: document.write() deletes all other existing HTML stuff.
+    document.write("You don't belong here!");
+}
+
+function nerdAlert(){
+    //Note: this is a popup
+    window.alert("You are in danger!");
+}
+
+function consoleDebug(){
+    //Check the output using developer tools
+    let x = 5;
+    let y = 6;
+    console.log(x+y);
+    console.log("\n");
+    var w = 5;
+    var v = 6;
+    console.log(w+v);
+}
