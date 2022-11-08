b974c6a496190cb72babd31ac7239dc1adab9899
Rohan Singh <rohan.b.singh54@gmail.com>
2022-11-08 10:50:17 -0500

    Added MySql connection to cwru-courses

--- Next.js Projects/cwru-courses/next.config.js
+++ Next.js Projects/cwru-courses/next.config.js
@@ -4,3 +4,13 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  env: {
+    'MYSQL_HOST': '127.0.0.1',
+    'MYSQL_PORT': '3306',
+    'MYSQL_DATABASE': {database_name},
+    'MYSQL_USER': {user_name},
+    'MYSQL_PASSWORD': {user_password},
+  }
+}
